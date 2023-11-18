c61977953129209a077bcb504fca7daa9d94e416
KlaudiuszWojtkowiak <klaudiusz.wojtkowiak@gmail.com>
2023-11-18 18:24:50 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # let-me-in
-A Startup Project for your database
+A Daobab Startup Project for your database
+
+# About
+This project may help you create Daobab connection to your database and perform complete Reverse Engineering for it.
+
+The project contains just two classes:
+-Application - a Spring Boot starter
+-Guide - your guide to check the connection and perform the reverse engineering.
+
+By default, the project contains client libraries to: MySQL, PosgreSql, H2
+
+# How to run?
+Take a look at the application.yml properties file.
+Put the proper parameters to connect to your database.
+
+Run the Application class
+
+Check out the console. Daobab connector should validate the database connection.
+If everyrthing is fine, you can find something like this:
+
+<code>
+<br>Connection OK. Database: MySQL version: 8.0 driver: MySQL Connector/J
+<br>User 'root' is allowed to read database content as follows: 
+<br>Catalog: cloud_job_manager (no schema)
+<br>Catalog: cross (no schema)
+<br>Catalog: information_schema (no schema)
+<br>Catalog: kwadrat (no schema)
+<br>Catalog: l2 (no schema)
+<br>Catalog: mdm (no schema)
+<br>Catalog: mysql (no schema)
+<br>Catalog: performance_schema (no schema)
+<br>Catalog: sakila (no schema)
+<br>Catalog: sys (no schema)
+<br>Catalog: testdb (no schema)
+<br>Catalog: world (no schema)
+</code>
+
+
+If the database connection is fine, you can perform the reverse engineering:
+
+Go to Guide class and init method.
+comment: step1_checkConnection()
+uncomment: step2_generateClasses();
+
+Go to step2_generateClasses() method:
+
+Decide where Daobab may write the classes:
+<code>generator.setPath("C:\\Daobab\\");</code>
+
+Decide which catalogie or schema you want to handle:
+<code>
+        generator.generateOnlyForCatalogues("catalogue");
+        generator.generateOnlyForSchemas("schema");
+</code>
+
+Otherwise, all catalogues and schemas will be recreated in classes.
+
+Next, run the application again.
+
+Generated output classes should be put into the project you are working on.
+
+
+
---
+++ src/main/java/io/daobab/hello/Guide.java
@@ -0,0 +1,64 @@
+package io.daobab.hello;
+
+import io.daobab.generator.DaobabGenerator;
+import io.daobab.generator.template.TemplateLanguage;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+@Configuration
+public class Guide {
+
+    @Value("${daobab.hello.url}")
+    private String url;
+    @Value("${daobab.hello.user}")
+    private String user;
+    @Value("${daobab.hello.pass}")
+    private String pass;
+    @Value("${daobab.hello.driverClassName}")
+    private String driverClassName;
+
+    protected final DaobabGenerator generator = new DaobabGenerator();
+
+
+    @PostConstruct
+    public void init() {
+        step1_checkConnection();
+//        step2_generateClasses();
+    }
+
+    public void step1_checkConnection() {
+        //STEP 1: check the database connection
+        generator.checkConnection(url, user, pass, getDriverClass());
+    }
+
+    public void step2_generateClasses() {
+        //STEP 2: gererate classes
+        generator.setLanguage(TemplateLanguage.JAVA);
+        generator.setPackage("io.daobab.hello.dao");
+        //where to write output classes
+        generator.setPath("C:\\Daobab\\");
+
+        //your databasase may contain many catalogues or schemas,
+        //it's printed at step 1
+        //here you can limit the scope to particular catalog and schema
+
+//        generator.generateOnlyForCatalogues("catalogue");
+//        generator.generateOnlyForSchemas("schema");
+
+        generator.reverseEngineering(url, user, pass, getDriverClass());
+
+
+        //At the end, don't forget to copy the generated classes to 'io.daobab.hello.dao' package
+        //and put the DataSource connection pool.
+    }
+
+    protected Class getDriverClass() {
+        try {
+            return Class.forName(driverClassName);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
